cfef0dd56f36840fbd5e54cfe74d4625dfaa33c8
Cryptopragmatic <tristan.alberio@gmail.com>
2021-10-12 17:43:13 +0800

    first solana program run

--- examples/tutorial/basic-0/programs/basic-0/src/lib.rs
+++ examples/tutorial/basic-0/programs/basic-0/src/lib.rs
@@ -1,14 +1,15 @@
-use anchor_lang::prelude::*;
+use anchor_lang::prelude::*;  // using anchor library
 //hello
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"); 
 
-#[program]
+#[program] // instruction handler, client entry point, rpc request handler
 mod basic_0 {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult { //Context which is a simple container for the currently executing, option result 
         Ok(())
     }
 }
 
-#[derive(Accounts)]
+#[derive(Accounts)] // all accounts specified for an instruction e.g addresses need for the program to execute
+//marks a struct containing all the accounts that must be specified for a given instruction
 pub struct Initialize {}
